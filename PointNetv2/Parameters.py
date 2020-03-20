1f975c37ee4e7198830d1c1cc5c3e20467f929a8
heatherwan <wlin@mail.uni-mannheim.de>
2020-03-20 13:53:12 +0000

    update exp-200epoches

--- PointNetv2/Parameters.py
+++ PointNetv2/Parameters.py
@@ -14,21 +14,21 @@ class Parameters:
             os.mkdir(self.logDir)
         if not os.path.isdir(self.logmodelDir):
             os.mkdir(self.logmodelDir)
-        self.TRAIN_FILES = os.path.join(self.dataDir, 'traindataset_dim4_480_3cat.hdf5')
-        self.TEST_FILES = os.path.join(self.dataDir, 'testdataset_dim4_160_3cat.hdf5')
+        self.TRAIN_FILES = os.path.join(self.dataDir, 'traindataset_dim4_480.hdf5')
+        self.TEST_FILES = os.path.join(self.dataDir, 'testdataset_dim4_160.hdf5')
 
-        self.outputClassN = 3
+        self.outputClassN = 4
         self.pointNumber = 1024
         self.dim = 4
 
         self.batchSize = 32
         self.testBatchSize = 32
-        self.max_epoch = 100
+        self.max_epoch = 200
         self.learningRate = 2e-3
 
         self.weighting_scheme = 'weighted'
         self.weight_scaler = 4  # 50
-        self.expName = f'exp103_point{self.pointNumber}_batch{self.batchSize}_dim{self.dim}_{self.weighting_scheme}'  # save model path
+        self.expName = f'exp203_point{self.pointNumber}_batch{self.batchSize}_out{self.outputClassN}_weighted{self.weight_scaler}'  # save model path
 
         self.momentum = 0.9
         self.optimizer = 'adam'  # or momentum
