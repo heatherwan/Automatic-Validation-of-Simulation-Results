3379a48c0eac473adffee35060156982e10e6019
heatherwan <wnln@festo.net>
2020-03-23 12:03:00 +0100

    add evaluation parameters

--- PointNetv2/Parameters.py
+++ PointNetv2/Parameters.py
@@ -4,41 +4,49 @@ import shutil
 
 class Parameters:
     def __init__(self):
-        self.gpu = '-1'
+        # ==============Network setting===========================
+        self.gpu = False
         self.model = 'pointnet_cls'
-        self.logmodelDir = os.path.join(os.getcwd(), 'logmodel')
-        self.logDir = os.path.join(os.getcwd(), 'log')
-        self.dataDir = os.path.join(os.getcwd(), 'datasets')
+        self.outputClassN = 4
+        self.pointNumber = 1024
+        self.dim = 4
+        self.batchSize = 32
+        self.testBatchSize = 32
+        self.max_epoch = 200
+        self.learningRate = 2e-3
+        self.momentum = 0.9
+        self.optimizer = 'adam'  # or momentum
+        self.decay_step = 20000  # 1 epoch 1000 step
+        self.decay_rate = 0.7
+        self.weighting_scheme = 'weighted'
+        self.weight_scaler = 4  # 50
 
+        # ==============Files setting===========================
+        self.logmodelDir = os.path.join(os.getcwd(), 'logmodel')
+        if not os.path.isdir(self.logmodelDir):
+            os.mkdir(self.logmodelDir)
+
+        self.logDir = os.path.join(os.getcwd(), 'log')
         if not os.path.isdir(self.logDir):
             os.mkdir(self.logDir)
             os.mkdir(os.path.join(self.logDir, 'train'))
             os.mkdir(os.path.join(self.logDir, 'test'))
+            os.mkdir(os.path.join(self.logDir, 'trainold'))
+            os.mkdir(os.path.join(self.logDir, 'testold'))
         else:
             for file in os.listdir(os.path.join(self.logDir, 'train')):
                 shutil.move(os.path.join(self.logDir, 'train', file), os.path.join(self.logDir, 'trainold', file))
             for file in os.listdir(os.path.join(self.logDir, 'test')):
                 shutil.move(os.path.join(self.logDir, 'test', file), os.path.join(self.logDir, 'testold', file))
 
-        if not os.path.isdir(self.logmodelDir):
-            os.mkdir(self.logmodelDir)
+        self.evallog = os.path.join(os.getcwd(), 'evallog')
+        if not os.path.isdir(self.evallog):
+            os.mkdir(self.evallog)
+
+        self.dataDir = os.path.join(os.getcwd(), 'datasets')
         self.TRAIN_FILES = os.path.join(self.dataDir, 'traindataset_dim4_480.hdf5')
         self.TEST_FILES = os.path.join(self.dataDir, 'testdataset_dim4_160.hdf5')
 
-        self.outputClassN = 4
-        self.pointNumber = 1024
-        self.dim = 4
-
-        self.batchSize = 32
-        self.testBatchSize = 32
-        self.max_epoch = 200
-        self.learningRate = 2e-3
-
-        self.weighting_scheme = 'weighted'
-        self.weight_scaler = 4  # 50
         self.expName = f'exp203_point{self.pointNumber}_batch{self.batchSize}_out{self.outputClassN}_weighted{self.weight_scaler}'  # save model path
 
-        self.momentum = 0.9
-        self.optimizer = 'adam'  # or momentum
-        self.decay_step = 20000  # 1 epoch 1000 step
-        self.decay_rate = 0.7
+        self.classes = {1: 'EM1', 2: 'EM3', 3: 'EM4', 0: 'Good'}
