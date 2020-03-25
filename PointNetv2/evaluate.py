259aac4cd7dd60d92142718778b5e3ed27277377
heatherwan <wnln@festo.net>
2020-03-25 15:08:57 +0100

    update script for add feature: distance to MinSF

--- PointNetv2/evaluate.py
+++ PointNetv2/evaluate.py
@@ -45,12 +45,12 @@ def log_string(out_str):
 
 def evaluate(num_votes):
     with tf.device(''):
-        pointclouds_pl, pointclouds_sf_pl, labels_pl = MODEL.placeholder_inputs_other(BATCH_SIZE, NUM_POINT)
+        pointclouds_pl, pointclouds_other_pl, labels_pl = MODEL.placeholder_inputs_other(BATCH_SIZE, NUM_POINT)
         is_training_pl = tf.compat.v1.placeholder(tf.bool, shape=())
         weights = tf.compat.v1.placeholder(tf.float32, [None])
 
         # simple model
-        pred, end_points = MODEL.get_model_other(pointclouds_pl, pointclouds_sf_pl, is_training_pl)
+        pred, end_points = MODEL.get_model_other(pointclouds_pl, pointclouds_other_pl, is_training_pl)
         if para.weighting_scheme == 'weighted':
             loss = MODEL.get_loss_weight(pred, labels_pl, end_points, weights)
         else:
@@ -72,7 +72,7 @@ def evaluate(num_votes):
     log_string("Model restored.")
 
     ops = {'pointclouds_pl': pointclouds_pl,
-           'pointclouds_sf_pl': pointclouds_sf_pl,
+           'pointclouds_other_pl': pointclouds_other_pl,
            'labels_pl': labels_pl,
            'is_training_pl': is_training_pl,
            'pred': pred,
@@ -120,9 +120,9 @@ def eval_one_epoch(sess, ops, num_votes=1):
     fout.write('no predict real\n')
 
     # load data
-    current_data, current_sf, current_label = provider.loadDataFile_other(para.TEST_FILES)
+    current_data, current_other, current_label = provider.loadDataFile_other(para.TEST_FILES)
     current_data = current_data[:, 0:NUM_POINT, :]
-    current_sf = current_sf[:, 0:NUM_POINT]
+    current_other = current_other[:, 0:NUM_POINT]
     current_label = np.squeeze(current_label)
     print(current_data.shape)
     weight_dict = weight_dict_fc(current_label, para)
@@ -144,7 +144,7 @@ def eval_one_epoch(sess, ops, num_votes=1):
             rotated_data = provider.rotate_point_cloud_by_angle(current_data[start_idx:end_idx, :, :],
                                                                 vote_idx / float(num_votes) * np.pi * 2)
             feed_dict = {ops['pointclouds_pl']: rotated_data,
-                         ops['pointclouds_sf_pl']: current_sf[start_idx:end_idx, :],
+                         ops['pointclouds_other_pl']: current_other[start_idx:end_idx, :],
                          ops['labels_pl']: current_label[start_idx:end_idx],
                          ops['is_training_pl']: is_training,
                          ops['weights']: batchWeight}
