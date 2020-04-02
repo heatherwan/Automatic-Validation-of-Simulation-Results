e0827b750ed8c88157e449caf960084ca7eb82c8
heatherwan <wnln@festo.net>
2020-04-02 14:44:52 +0200

    add dcgnn and the modification needed

--- PointNetv2/utils/tf_util.py
+++ PointNetv2/utils/tf_util.py
@@ -88,7 +88,7 @@ def conv1d(inputs,
     Variable tensor
   """
     with tf.compat.v1.variable_scope(scope) as sc:
-        num_in_channels = inputs.get_shape()[-1].value
+        num_in_channels = inputs.get_shape()[-1]  # .value
         kernel_shape = [kernel_size,
                         num_in_channels, num_output_channels]
         kernel = _variable_with_weight_decay('weights',
@@ -148,7 +148,7 @@ def conv2d(inputs,
   """
     with tf.compat.v1.variable_scope(scope) as sc:
         kernel_h, kernel_w = kernel_size
-        num_in_channels = inputs.get_shape()[-1].value
+        num_in_channels = inputs.get_shape()[-1]  # .value
         kernel_shape = [kernel_h, kernel_w,
                         num_in_channels, num_output_channels]
         kernel = _variable_with_weight_decay('weights',
@@ -211,7 +211,7 @@ def conv2d_transpose(inputs,
   """
     with tf.compat.v1.variable_scope(scope) as sc:
         kernel_h, kernel_w = kernel_size
-        num_in_channels = inputs.get_shape()[-1].value
+        num_in_channels = inputs.get_shape()[-1]  # .value
         kernel_shape = [kernel_h, kernel_w,
                         num_output_channels, num_in_channels]  # reversed to conv2d
         kernel = _variable_with_weight_decay('weights',
@@ -230,9 +230,9 @@ def conv2d_transpose(inputs,
             return dim_size
 
         # caculate output shape
-        batch_size = inputs.get_shape()[0].value
-        height = inputs.get_shape()[1].value
-        width = inputs.get_shape()[2].value
+        batch_size = inputs.get_shape()[0]  # .value
+        height = inputs.get_shape()[1]  # .value
+        width = inputs.get_shape()[2]  # .value
         out_height = get_deconv_dim(height, stride_h, kernel_h, padding)
         out_width = get_deconv_dim(width, stride_w, kernel_w, padding)
         output_shape = [batch_size, out_height, out_width, num_output_channels]
@@ -289,7 +289,7 @@ def conv3d(inputs,
   """
     with tf.compat.v1.variable_scope(scope) as sc:
         kernel_d, kernel_h, kernel_w = kernel_size
-        num_in_channels = inputs.get_shape()[-1].value
+        num_in_channels = inputs.get_shape()[-1]  # .value
         kernel_shape = [kernel_d, kernel_h, kernel_w,
                         num_in_channels, num_output_channels]
         kernel = _variable_with_weight_decay('weights',
@@ -335,7 +335,7 @@ def fully_connected(inputs,
     Variable tensor of size B x num_outputs.
   """
     with tf.compat.v1.variable_scope(scope) as sc:
-        num_input_units = inputs.get_shape()[-1].value
+        num_input_units = inputs.get_shape()[-1]  # .value
         weights = _variable_with_weight_decay('weights',
                                               shape=[num_input_units, num_outputs],
                                               use_xavier=use_xavier,
@@ -472,7 +472,7 @@ def batch_norm_template(inputs, is_training, scope, moments_dims, bn_decay):
       normed:        batch-normalized maps
   """
     with tf.compat.v1.variable_scope(scope) as sc:
-        num_channels = inputs.get_shape()[-1].value
+        num_channels = inputs.get_shape()[-1]  # .value
         beta = tf.Variable(tf.constant(0.0, shape=[num_channels]),
                            name='beta', trainable=True)
         gamma = tf.Variable(tf.constant(1.0, shape=[num_channels]),
@@ -510,7 +510,7 @@ def batch_norm_dist_template(inputs, is_training, scope, moments_dims, bn_decay)
       normed:        batch-normalized maps
   """
     with tf.compat.v1.variable_scope(scope) as sc:
-        num_channels = inputs.get_shape()[-1].value
+        num_channels = inputs.get_shape()[-1]  # .value
         beta = _variable_on_cpu('beta', [num_channels], initializer=tf.compat.v1.zeros_initializer())
         gamma = _variable_on_cpu('gamma', [num_channels], initializer=tf.compat.v1.ones_initializer())
 
@@ -686,9 +686,9 @@ def get_edge_feature(point_cloud, nn_idx, k=20):
     point_cloud_central = point_cloud
 
     point_cloud_shape = point_cloud.get_shape()
-    batch_size = point_cloud_shape[0].value
-    num_points = point_cloud_shape[1].value
-    num_dims = point_cloud_shape[2].value
+    batch_size = point_cloud_shape[0]  # .value
+    num_points = point_cloud_shape[1]  # .value
+    num_dims = point_cloud_shape[2]  # .value
 
     idx_ = tf.range(batch_size) * num_points
     idx_ = tf.reshape(idx_, [batch_size, 1, 1])
