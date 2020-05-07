c973b180ef8dced56524f41e9017804177d5e896
heatherwan <wnln@festo.net>
2020-05-07 10:21:36 +0200

    add variablename

--- PointNetv2/evaluate_ldgcnn.py
+++ PointNetv2/evaluate_ldgcnn.py
@@ -70,6 +70,7 @@ def evaluate():
         # Add ops to save and restore all the variables.
         variable_names = [v.name for v in tf.compat.v1.global_variables()]
         print(variable_names)
+        input('press')
         variables = tf.compat.v1.global_variables()
         # Variables before #43 belong to the feature extractor.
         saver_cnn = tf.compat.v1.train.Saver(variables[0:44])
@@ -81,7 +82,7 @@ def evaluate():
     config.gpu_options.allow_growth = True
     config.allow_soft_placement = True
     config.log_device_placement = True
-    sess = tf.compat.v1.Session(config=config)
+    # sess = tf.compat.v1.Session(config=config)
 
     ops = {'pointclouds_pl': pointclouds_pl,
            'pointclouds_other_pl': pointclouds_other_pl,
