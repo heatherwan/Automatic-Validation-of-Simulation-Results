90c31fd288b2655720d2d37276c07e65557198be
heatherwan <wnln@festo.net>
2020-05-19 15:55:56 +0200

    update to L11

--- PointNetv2/models/densepoint_tf.py
+++ PointNetv2/models/densepoint_tf.py
@@ -39,23 +39,28 @@ def get_model_other(point_cloud, is_training, bn_decay=None):
 
     # first stage: 1 PPool, 3 EnhancedPConv
     all_xyz, all_points = densepoint_module(l0_xyz, l0_points, is_training, bn_decay,
-                                            npoint=512, radius=0.25, nsample=64, mlp=para.k_add*4,
+                                            npoint=512, radius=0.25, nsample=64, mlp=para.k_add * 4,
                                             scope='PPool1', ppool=True)
-
-    # for i in range(4):  # B 128 1 93 -> 24
-    #     all_xyz, all_points = pointnet_sa_module_msg(all_xyz, all_points, is_training, bn_decay,
-    #                                                  npoint=512, radius=0.39, nsample=32, mlp=para.k_add*4,
-    #                                                  group_num=para.group_num,
-    #                                                  scope=f'PConv1_{i + 1}')
 
     # second stage: 2 PPool, 3 EnhancedPConv
     all_xyz, all_points = densepoint_module(all_xyz, all_points, is_training, bn_decay,
-                                            npoint=128, radius=0.32, nsample=64, mlp=para.k_add*4-3,
+                                            npoint=256, radius=0.27, nsample=64, mlp=para.k_add * 4 - 3,
+                                            scope='PPool2', ppool=True)
+
+    for i in range(4):  # B 128 1 93 -> 24
+        all_xyz, all_points = densepoint_module(all_xyz, all_points, is_training, bn_decay,
+                                                npoint=256, radius=0.32, nsample=16, mlp=para.k_add * 4,
+                                                group_num=para.group_num,
+                                                scope=f'PConv1_{i + 1}')
+
+    # second stage: 2 PPool, 3 EnhancedPConv
+    all_xyz, all_points = densepoint_module(all_xyz, all_points, is_training, bn_decay,
+                                            npoint=128, radius=0.32, nsample=64, mlp=para.k_add * 4 - 3,
                                             scope='PPool3', ppool=True)
 
     for i in range(4):  # B 128 1 93 -> 24
         all_xyz, all_points = densepoint_module(all_xyz, all_points, is_training, bn_decay,
-                                                npoint=128, radius=0.39, nsample=16, mlp=para.k_add*4,
+                                                npoint=128, radius=0.39, nsample=16, mlp=para.k_add * 4,
                                                 group_num=para.group_num,
                                                 scope=f'PConv2_{i + 1}')
 
