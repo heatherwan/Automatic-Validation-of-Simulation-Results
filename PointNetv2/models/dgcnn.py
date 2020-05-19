b5320f16a80c3fba6ac402f3af7f634f52eed73a
heatherwan <wnln@festo.net>
2020-05-19 10:50:18 +0200

    add BN after concat

--- PointNetv2/models/dgcnn.py
+++ PointNetv2/models/dgcnn.py
@@ -40,6 +40,8 @@ def get_model_other(point_cloud, is_training, bn_decay=None):
 
     # # 2. graph for first EdgeConv with transform(x,y,z), SF, distance, minSF
     point_cloud_all = tf.concat(axis=2, values=[point_cloud_transform, point_cloud[:, :, 3:]])
+    point_cloud_all = tf_util.batch_norm_for_conv2d(point_cloud_all, is_training=is_training,
+                                                    scope='BNConcat', bn_decay=bn_decay)
 
     adj_matrix = tf_util.pairwise_distance(point_cloud_all)  # B N C=6
     nn_idx = tf_util.knn(adj_matrix, k=para.k)
