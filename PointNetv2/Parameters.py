2d5eca12c4096487c613b7ea578599dfd38a57d4
heatherwan <wnln@festo.net>
2020-06-24 14:28:08 +0200

    prepare for 3 class test

--- PointNetv2/Parameters.py
+++ PointNetv2/Parameters.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import shutil
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
@@ -10,7 +9,7 @@ sys.path.append(os.path.join(BASE_DIR, 'models'))
 class Parameters:
     def __init__(self, evaluation=False):
         # ==============Network setting===========================
-        self.gpu = False
+        self.gpu = True
         self.model = 'dgcnn'  # 'pointnet_cls'# 'dgcnn' # 'lgdcnn' # 'lgdcnn_cls' #'densepoint_tf'
         self.outputClassN = 4
         self.pointNumber = 1024
@@ -27,23 +26,28 @@ class Parameters:
         # parameters for loss setting
         self.binary_loss = True
         self.loss_smoothing = 0.05
-        self.fine_coarse_mapping = [[1.0, 0.0],
-                                    [0.0, 1.0],
-                                    [0.0, 1.0],
-                                    [0.0, 1.0]]
+        if self.outputClassN == 4:
+            self.fine_coarse_mapping = [[1.0, 0.0],
+                                        [0.0, 1.0],
+                                        [0.0, 1.0],
+                                        [0.0, 1.0]]
+        elif self.outputClassN ==3:
+            self.fine_coarse_mapping = [[1.0, 0.0],
+                                        [0.0, 1.0],
+                                        [0.0, 1.0]]
 
         # parameters for cross validation
         self.validation = False
         self.split_num = 5
 
         # parameter for dgcnn and ldgcnn
-        self.k = 20
+        self.k = 10
 
         # parameters for densepoint
         self.group_num = 4
         self.k_add = 24
 
-        self.expName = f'exp433_point{self.pointNumber}_batch{self.batchSize}_out{self.outputClassN}' \
+        self.expName = f'exp607_point{self.pointNumber}_batch{self.batchSize}_out{self.outputClassN}' \
                        f'_smooth{self.loss_smoothing}'  # save model path
 
         # ==============Files setting===========================
@@ -60,8 +64,10 @@ class Parameters:
                 os.mkdir(self.evallog)
 
         self.dataDir = os.path.join(BASE_DIR, 'datasets')
-        self.TRAIN_FILES = [os.path.join(self.dataDir, 'traindataset_651_1024_dim6_normal.hdf5')]
-        self.TEST_FILES = [os.path.join(self.dataDir, 'testdataset_163_1024_dim6_normal.hdf5')]
+        self.TRAIN_FILES = [os.path.join(self.dataDir, 'traindataset_814_3class_1024_dim6_normal.hdf5')]
+        self.TEST_FILES = [os.path.join(self.dataDir, 'testdataset_855_3class_1024_dim6_normal.hdf5')]
 
-        self.classes = {1: 'EM1_contact', 2: 'EM3_radius', 3: 'EM4_hole', 0: 'Good'}
-        # self.classes = {1: 'EM13_contactradius', 2: 'EM4_hole', 0: 'Good'}
+        if self.outputClassN == 4:
+            self.classes = {1: 'EM1_contact', 2: 'EM3_radius', 3: 'EM4_hole', 0: 'Good'}
+        elif self.outputClassN == 3:
+            self.classes = {1: 'EM1_contact', 2: 'EM4_hole', 0: 'Good'}
