modify the coordinate and SF wrong index

PointNetv2/evaluate_multiangle.py
@@ -117,7 +117,7 @@ def eval_one_epoch(sess, ops):
         all_pred_prob = np.zeros((cur_batch_data.shape[0], para.outputClassN))
 
         for vote_idx in range(para.num_votes):
-            cur_batch_data[:, :, :3] = provider.rotate_point_cloud_by_angle(cur_batch_data[:, :, :3],
+            cur_batch_data[:, :, 1:] = provider.rotate_point_cloud_by_angle(cur_batch_data[:, :, 1:],
                                                                             vote_idx / float(
                                                                                 para.num_votes) * np.pi * 2)
             feed_dict = {ops['pointclouds_pl']: cur_batch_data,

PointNetv2/models/ldgcnn.py
@@ -36,7 +36,7 @@ def get_model_other(point_cloud, is_training, bn_decay=None):
     """
 
     end_points = {}
-    minSF = tf.reshape(tf.math.argmin(point_cloud[:, :, 3], axis=1), (-1, 1))
+    minSF = tf.reshape(tf.math.argmin(point_cloud[:, :, 0], axis=1), (-1, 1))
     batch_size = point_cloud.get_shape()[0]  # .value
 
     # # 1. graph for first EdgeConv B N C=6